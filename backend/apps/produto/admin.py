763110edf72e90cfb5c5d497e3db3ac69ad9b958
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-03-18 11:32:48 -0300

    Commit Inicial. Configuracao do Django, Banco de dados, e Painel Admin foi feita.

---
+++ backend/apps/produto/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import ProdutoEletrInf, PrecoInf
+
+# -------------------------------------------------------
+# Arquivo responsável por registrar (ligar) os modelos
+# (tabelas), ao painel admin e assim podermos acessa-las,
+# edita-las e apagar elas por lá.
+# -------------------------------------------------------
+
+# Configurações que determinam como esse modelo deve se
+# apresentar e comportam quando tiver sendo acessa-da
+# através do painel admin.
+class ProdutoEletrInfAdmin(admin.ModelAdmin):
+    # O modelo que essa configuração se aplica
+    model = ProdutoEletrInf
+    # Campos que devem apenas ser vistos, e não
+    # alterados no painel admin.
+    readonly_fields = ("plataforma", "data_criacao")
+    # Campos que podem ser mostrados ao olhar
+    # por esse modelo no painel admin.
+    list_display = ("nome", "imagem", "url_produto", "plataforma", "data_criacao", "monitorar")
+    # Campos que podem ser usados para filtrar
+    # resultados encontrados na busca.
+    list_filter = ("monitorar", "plataforma", "data_criacao")
+    # Os campos desse modelo (tabela) que podem ser
+    # usados para pesquisar por esse modelo.
+    # (Nome dele, e o site que foi encontrado)
+    search_fields = ("nome", "plataforma")
+
+class PrecoInfAdmin(admin.ModelAdmin):
+    model = PrecoInf
+    readonly_fields = ("produto_referencia", "data_preco")
+    list_display = ("produto_referencia", "valor", "data_preco")
+    list_filter = ("data_preco", "valor")
+    search_fields = ("produto_referencia", "data_preco")
+
+
+# Liga as nossas configurações do painel admin a esta classe.
+# E a torna acessível pelo admin também.
+admin.site.register(ProdutoEletrInf, ProdutoEletrInfAdmin)
+admin.site.register(PrecoInf, PrecoInfAdmin)
---
+++ backend/apps/produto/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+
+# -------------------------------------------------------
+# Django automaticamente cria um campo id. Que é inteiro,
+# unico e que se auto-incrementa ao longo do tempo, ao
+# criar um modelo.
+# -------------------------------------------------------
+
+# Modelo responsável por armazenar todas as informações
+# sobre um produto eletrônico. (Menos preço, por questão
+# de organização)
+class ProdutoEletrInf(models.Model):
+    # Define o nome da tabela que será salvo no banco esse modelo
+    class Meta:
+        db_table = "produto"
+
+    # Armazena o nome do produto (máximo 100 caracteres)
+    nome = models.CharField(max_length=100)
+    # url da imagem desse produto
+    imagem = models.CharField(max_length=255)
+    # url do produto, mas com TextField pois ele não tem
+    # limite de caracteres como o CharField
+    url_produto = models.TextField()
+    # O site em que esse produto foi encontrado (pichau,
+    # kabum e etc) O verbose_name muda o nome que aparece
+    # esse campo ao ve-lo no admin.
+    plataforma = models.CharField(verbose_name="website", max_length=100)
+    # Armazena a data e o horário exato em que esse modelo
+    # foi criado. (Atualizações futuras contam como edição)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    # Marca se esse produto deve ser monitorado ou não.
+    # Para pode desativar monitoramento de produtos que
+    # não importam. Padrão é True (Sim).
+    monitorar = models.BooleanField(default=True)
+
+    # Retorna o que deve aparecer para identificar esse
+    # produto ao retornar uma busca.
+    def __str__(self):
+        # Tudo em uma linha por performance. (Se tivesse
+        # milhares de linha ia armazenar o retorno de
+        # cada um em uma variavel só pra apagar depois
+        return f"{self.nome}({self.plataforma})"
+
+# Modelo responsável por representar o preço de um produto
+# cadastrado, em um determinado dia e horário.
+# (Para poder monitorar a variação do preço de um produto
+# ao longo do tempo)
+class PrecoInf(models.Model):
+    # Define o nome da tabela que será salvo no banco esse modelo
+    class Meta:
+        db_table = "preco_produto"
+
+    # Cria uma chave estrangeira que aponta para produto, ou seja
+    # preco precisa ter um produto. Mas um produto pode ter varios
+    # precos. O on_delete como CASCADE faz com que ao apagar um
+    # produto todos os seus precos sejam apagos juntos com ele.
+    # verbose_name faz com que no admin aparece um nome mais
+    # descritivo.
+    # (Django automaticamente coloca todas as chaves estrangeiras
+    # no final na hora de criar a tabela para não dar erro)
+    produto_referencia = models.ForeignKey(ProdutoEletrInf, on_delete=models.CASCADE, verbose_name="Produto referente")
+    # Armazena como string o preco ao inves de float para facilitar
+    # o uso na API, e na hora de notificar os produtos para pegar
+    # seu valor. Também elimina problema de extorar limite de número
+    # para preços grandes, e é utiliza menos memória armazenar um
+    # número grande como sting. (Maioria dos usos é em string, e
+    # se precisar é só converter)
+    valor = models.CharField(max_length=50, verbose_name="Preço do produto")
+    # Ao criar um modelo de preço ele automicamente pega a data e
+    # hora de criação e coloca nesse campo. Para poder
+    # posteriormente comprar os preços do produto em intervalos
+    # de tempo. (Pega a data de criação e não de edição, pois esse
+    # modelo não faz sentido e não deve ser editado)
+    data_preco = models.DateTimeField(auto_now_add=True, verbose_name="Data do preço")
+
+    # Retorna o que deve aparecer para identificar esse
+    # preço ao retornar uma busca.
+    def __str__(self):
+        # Tudo em uma linha por performance. (Se tivesse
+        # milhares de linha ia armazenar o retorno de
+        # cada um em uma variavel só pra apagar depois
+        return f"{self.produto_referencia} - R${self.valor}({self.data_preco})"
