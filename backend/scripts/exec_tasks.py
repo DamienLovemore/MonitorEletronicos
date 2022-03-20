5bc6578aceef516e53c0c06f37a73d412ff71d89
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-03-20 15:57:30 -0300

    Configuracao feita para rodar tarefas dos bots continuamente de maneira eficiente, e testada para rodar todos os bots de kabum novidades.

--- backend/scripts/exec_tasks.py
+++ backend/scripts/exec_tasks.py
@@ -1,4 +1,11 @@
-from apps.monitor.integrations.discord.notifier import DiscordNotify
+import continuous_threading
+from time import sleep
+
+#Kabum
+from apps.monitor.kabum.novidades.kabum_novidades_games import KabumNovidadesGames
+from apps.monitor.kabum.novidades.kabum_novidades_hardware import KabumNovidadesHardware
+from apps.monitor.kabum.novidades.kabum_novidades_computadores import KabumNovidadesComputadores
+from apps.monitor.kabum.novidades.kabum_novidades_smartphones import KabumNovidadesSmartphones
 
 # ---------------------------------------------------------------------------
 # Arquivo responsável por rodar todos os bots de cada canal do Discord a cada
@@ -7,6 +14,38 @@ from apps.monitor.integrations.discord.notifier import DiscordNotify
 # respondendo a usuários.
 # ---------------------------------------------------------------------------
 
+def task_kabum_novidades_games():
+    kabum_novidades_games = KabumNovidadesGames()
+    while True:
+        kabum_novidades_games.get_items()
+        sleep(8)
+
+def task_kabum_novidades_hardware():
+    kabum_novidades_hardware = KabumNovidadesHardware()
+    while True:
+        kabum_novidades_hardware.get_items()
+        sleep(8)
+
+def task_kabum_novidades_computadores():
+    kabum_novidades_computadores = KabumNovidadesComputadores()
+    while True:
+        kabum_novidades_computadores.get_items()
+        sleep(8)
+
+def task_kabum_novidades_smartphones():
+    kabum_novidades_smartphones = KabumNovidadesSmartphones()
+    while True:
+        kabum_novidades_smartphones.get_items()
+        sleep(8)
+
 def run():
-    # Placeholder
-    DiscordNotify.send_message_test()
+    # continuous_threading é uma biblioteca especializa em rodar threads
+    # continuamente. Ela possue por padrão diversos recursos, e ferramentas
+    # para otimização de threads que rodam em loop. E mecanismo para que ao
+    # encerrar as tasks seus recursos sejam liberados corretamente.
+
+    ### Kabum ###
+    continuous_threading.Thread(target=task_kabum_novidades_games).start()
+    continuous_threading.Thread(target=task_kabum_novidades_hardware).start()
+    continuous_threading.Thread(target=task_kabum_novidades_computadores).start()
+    continuous_threading.Thread(target=task_kabum_novidades_smartphones).start()
