dab0299849c23c85c190fe1a2f9aed4deb7b365e
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-03-23 08:13:30 -0300

    Script de ofertas para kabum foi feito e testado, e script de hist_precos foi melhorado.

--- backend/scripts/exec_tasks.py
+++ backend/scripts/exec_tasks.py
@@ -6,6 +6,7 @@ from apps.monitor.kabum.novidades.kabum_novidades_games import KabumNovidadesGam
 from apps.monitor.kabum.novidades.kabum_novidades_hardware import KabumNovidadesHardware
 from apps.monitor.kabum.novidades.kabum_novidades_computadores import KabumNovidadesComputadores
 from apps.monitor.kabum.novidades.kabum_novidades_smartphones import KabumNovidadesSmartphones
+from apps.monitor.kabum.ofertas.kabum_ofertas import KabumOfertas
 from apps.monitor.kabum.hist_precos.kabum_hist_precos import KabumHistPrecos
 
 # ---------------------------------------------------------------------------
@@ -19,27 +20,37 @@ def task_kabum_novidades_games():
     kabum_novidades_games = KabumNovidadesGames()
     while True:
         kabum_novidades_games.get_items()
-        sleep(8)
+        sleep(60)
 
 def task_kabum_novidades_hardware():
     kabum_novidades_hardware = KabumNovidadesHardware()
     while True:
         kabum_novidades_hardware.get_items()
-        sleep(8)
+        sleep(60)
 
 def task_kabum_novidades_computadores():
     kabum_novidades_computadores = KabumNovidadesComputadores()
     while True:
         kabum_novidades_computadores.get_items()
-        sleep(8)
+        sleep(60)
 
 def task_kabum_novidades_smartphones():
     kabum_novidades_smartphones = KabumNovidadesSmartphones()
     while True:
         kabum_novidades_smartphones.get_items()
-        sleep(8)
+        sleep(60)
+
+def task_kabum_ofertas():
+    kabum_ofertas = KabumOfertas()
+    while True:
+        kabum_ofertas.get_items()
+        sleep(100)
 
 def task_kabum_monitorar_precos():
+    # Tempo suficiente das task de novidades, e ofertas terem feito scraping
+    # e criado seus modelos. Para que não de interferencia com esse.
+    # (Pois faz muita requisicao, e uso do banco)
+    sleep(20)
     kabum_monitor_precos = KabumHistPrecos()
     kabum_monitor_precos.get_items()
 
@@ -56,6 +67,9 @@ def run():
     continuous_threading.Thread(target=task_kabum_novidades_hardware).start()
     continuous_threading.Thread(target=task_kabum_novidades_computadores).start()
     continuous_threading.Thread(target=task_kabum_novidades_smartphones).start()
+
+    # -> Ofertas
+    continuous_threading.Thread(target=task_kabum_ofertas).start()
 
     # -> Monitorar Preços
     # Como deve rodar uma vez só por dia, usado o threading normal
